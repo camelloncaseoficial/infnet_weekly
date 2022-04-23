d02b6b04486a4e6ec1982ca8afd89151bda98b32
Francisco Alves Camello Neto <franciscocamellon@gmail.com>
2022-04-23 00:41:11 -0300

    Finished moshi migration

--- app/src/main/java/com/camelloncase/infnetweekly/viewmodel/NotificationViewModel.kt
+++ app/src/main/java/com/camelloncase/infnetweekly/viewmodel/NotificationViewModel.kt
@@ -11,45 +11,12 @@ import java.util.*
 
 class NotificationViewModel(private val repository: NotificationApiRepository): ViewModel() {
 
-    val apiResponse: MutableLiveData<Response<Notification>> = MutableLiveData()
-    val notificationById: MutableLiveData<Response<Notification>> = MutableLiveData()
-    val allNotificationsByYear: MutableLiveData<Response<List<Notification>>> = MutableLiveData()
     val allNotificationsByWeek: MutableLiveData<Response<List<Notification>>> = MutableLiveData()
-    val allNotificationsByQueryMap: MutableLiveData<Response<List<Notification>>> = MutableLiveData()
-
-    fun getNotification() {
-        viewModelScope.launch {
-            val response = repository.getNotification()
-            apiResponse.value = response
-
-        }
-    }
-
-    fun getNotificationById(uuid: UUID) {
-        viewModelScope.launch {
-            val response = repository.getNotificationById(uuid)
-            notificationById.value = response
-        }
-    }
 
     fun getNotificationByWeek(begin: String, finish: String) {
         viewModelScope.launch {
             val response = repository.getNotificationByWeek(begin, finish)
             allNotificationsByWeek.value = response
-        }
-    }
-
-    fun getAllNotificationByYear(year: Int, sort: String, order: String) {
-        viewModelScope.launch {
-            val response = repository.getAllNotificationByYear(year, sort, order)
-            allNotificationsByYear.value = response
-        }
-    }
-
-    fun getAllNotificationByQueryMap(year: Int, options: Map<String, String>) {
-        viewModelScope.launch {
-            val response = repository.getAllNotificationByQueryMap(year, options)
-            allNotificationsByQueryMap.value = response
         }
     }
 
